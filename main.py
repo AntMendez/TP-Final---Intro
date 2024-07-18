7941dc43c3dec9c69885012cca8946b052ae0a0f
IgnacioFranciulli <ifranciulli@fi.uba.ar>
2024-07-17 21:11:36 -0300

    Agrego barra de busqueda con sus respectivos endpoints.

--- main.py
+++ main.py
@@ -233,6 +233,32 @@ def modificar_libro(id_libro):
         return jsonify({'message': 'Internal server error'}), 500
     
 
+@app.route("/catalogo/filtrar/<nombre>")
+def filtrar_by_nombre(nombre):
+    try:
+        libros_data =[]
+        busqueda = "%{}%".format(nombre)
+        libros=Libro.query.filter(Libro.nombre.ilike(busqueda)).all()
+        for libro in libros:
+            print(libro.nombre)
+            libro_data ={
+                    'id': libro.id,
+                    'nombre': libro.nombre,
+                    'autor': libro.autor,
+                    'img': libro.img,
+                    'pdf': libro.pdf,
+                    'descripcion': libro.descripcion,
+                    'categoria': libro.categoria,
+                    'puntuacion':libro.puntuacion
+                }
+            libros_data.append(libro_data)
+        #return jsonify({'libros':libros_data})
+        return render_template('libros.html', data= libros_data)
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+           
+
 @app.route("/catalogo/<id_libro>", methods = ["DELETE"])
 def borrar_libro(id_libro):
     try:
